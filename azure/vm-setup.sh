6396934af5c683b7602629e4b427319fb98ef291
Eleazar Alday García <eleazaraldayg@gmail.com>
2026-07-27 13:01:50 -0600

    Setup files 2

---
+++ azure/vm-setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+sudo apt update
+
+echo "Instalando stack..."
+
+sudo apt install default-mysql-server python3 python3-pip python3-venv git;
+
+echo "Clonando proyecto de GitHub..."
+
+mkdir dev && cd dev && git clone https://github.com/EleazarAldayG/app-asistencia-con-fallas.git && app-asistencia-con-fallas
+
+echo "Proyecto clonado! Descargando requirements"
+
+python3 -m venv venv && source venv/bin/activate && pip install -r requirements.txt;
+
+echo "Creando servicios para inicio automatico"
+
+sudo cp api-service-setup.ini /etc/systemd/system/myapi.service;
+sudo systemctl enable mysql;
+sudo system start mysql;
+sudo systemctl daemon-reload;
+sudo systemctl start myapi;
+sudo systemctl enable myapi;
+
+# Path to your SQL file – adjust if it's in a different folder
+SQL_FILE="./db-setup.sql"
+
+echo "Checking MySQL installation..."
+
+echo "Executing SQL script: $SQL_FILE"
+sudo mysql < "$SQL_FILE"
+
+echo "Database setup completed successfully."
