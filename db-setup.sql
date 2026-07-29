390714e5ebf697aa77d847ce51410fed6d7ab78d
Eleazar Alday García <eleazaraldayg@gmail.com>
2026-07-29 16:57:15 -0600

    Rename database from myappdb to attendance_db

--- db-setup.sql
+++ db-setup.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS myappdb;
+CREATE DATABASE IF NOT EXISTS attendance_db;
 CREATE USER 'attendance_user'@'localhost' IDENTIFIED BY 'secure_password';
 GRANT ALL PRIVILEGES ON attendance_db.* TO 'attendance_user'@'localhost';
 FLUSH PRIVILEGES;
