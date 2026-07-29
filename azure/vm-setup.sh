8044ff25aad8f49618c20197b692d239d16e5534
Eleazar Alday García <eleazaraldayg@gmail.com>
2026-07-29 14:11:37 -0600

    Setup files 7

--- azure/vm-setup.sh
+++ azure/vm-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# sudo curl -fsSL https://raw.githubusercontent.com/EleazarAldayG/app-asistencia-con-fallas/refs/heads/main/azure/vm-setup.sh -o /home/dev/vm-setup.sh && chmod +x /home/dev/vm-setup.sh && chown tdp:tdp /home/dev/vm-setup.sh && sudo -u tdp bash /home/dev/vm-setup.sh > /home/dev/setup.log 2>&1
+# cd ~ && mkdir dev && curl -fsSL https://raw.githubusercontent.com/EleazarAldayG/app-asistencia-con-fallas/refs/heads/main/azure/vm-setup.sh -o ~/dev/vm-setup.sh && sudo chmod +x ~/dev/vm-setup.sh && cd dev/ && sudo ./vm-setup.sh > ~/setup.log
 sudo apt update
 
 echo "Instalando stack..."
@@ -8,7 +8,7 @@ sudo apt install default-mysql-server python3 python3-pip python3-venv git
 
 echo "Clonando proyecto de GitHub..."
 
-cd /home && sudo mkdir dev && cd dev && git clone https://github.com/EleazarAldayG/app-asistencia-con-fallas.git && cd app-asistencia-con-fallas
+cd ~/dev/ && git clone https://github.com/EleazarAldayG/app-asistencia-con-fallas.git && cd app-asistencia-con-fallas
 
 echo "Proyecto clonado! Descargando requirements"
 
@@ -16,6 +16,7 @@ python3 -m venv .venv && source .venv/bin/activate && pip install -r requirement
 
 echo "Creando servicios para inicio automatico"
 
+sed -i "s/userdir/$USER/g" api-service-setup.ini
 sudo cp api-service-setup.ini /etc/systemd/system/myapi.service
 sudo systemctl daemon-reload
 sudo systemctl enable mysql
@@ -23,13 +24,8 @@ sudo systemctl start mysql
 sudo systemctl start myapi
 sudo systemctl enable myapi
 
-set -e
-
-SQL_FILE="./home/dev/app-asistencia-con-fallas/db-setup.sql"
-
 echo "Checking MySQL installation..."
 
-echo "Executing SQL script: $SQL_FILE"
-sudo mysql < "$SQL_FILE" 2>&1
+sudo mysql < ./db-setup.sql 2>&1
 
 echo "Database setup completed successfully."
